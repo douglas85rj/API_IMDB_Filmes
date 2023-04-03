45cfe5712f0e5f88665f72ece1a34c3d43cd4e71
douglas85rj <douglas85rj@gmail.com>
2023-04-03 13:03:40 -0300

    Conexão_API_IMDB

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     /**
@@ -9,7 +14,13 @@ public class App {
        
         //Coletar os dados do servidor IMDB (Top 250 filmes)
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-         HttpClient client = HttpClient.newHttpClient();
+        URI endereco = URI.create(url);
+         var client = HttpClient.newHttpClient();
+        var request =  HttpRequest.newBuilder(endereco).GET().build();
+         HttpResponse<String> response =  client.send(request, BodyHandlers.ofString());
+         String body = response.body();
+         System.out.println(body);
+       
         //Filtar algumas informações da API (título, poster, classificação)
 
 
